b881fd40f737c9177b58f311a744bcf0f1ce813d
Steve Walker <Steve.Walker@fisglobal.com>
2025-04-11 13:43:26 -0400

    settings.

--- settings/common.py
+++ settings/common.py
@@ -224,8 +224,8 @@ MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 # The absolute url is mandatory because attachments
 # urls depends on it. On production should be set
 # something like https://media.taiga.io/
-MEDIA_URL = "http://localhost:8000/media/"
-STATIC_URL = "http://localhost:8000/static/"
+MEDIA_URL = "/media/"
+STATIC_URL = "/static/"
 
 # Static configuration.
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
